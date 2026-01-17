65c8ff537c04396199f89edba36214101183e21d
josephodongo99@gmail.com <josephochieng@github.com>
2026-01-17 06:50:45 -0800

    Move files into wdd231 subfolder for specific URL path

---
+++ wdd231/scripts/getdates.js
@@ -0,0 +1,6 @@
+function showLastModified()  {
+    const lastModified = new Date(document.lastModified);
+    const options = { year: 'numeric', month: 'long', day: 'numeric', hour: '2-digit', minute: '2-digit', second: '2-digit' };
+    const formattedDate = lastModified.toLocaleDateString('en-US', options);
+    document.getElementById('lastModified').textContent = `Last Modified: ${formattedDate}`;
+}
