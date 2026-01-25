1a4c74f98ebb96c52d1233dfced94278023e0795
josephochiengodongo <josephodongo99@gmail.com>
2026-01-25 03:15:00 +0300

    effect

--- chamber/scripts/getdates.js
+++ chamber/scripts/getdates.js
@@ -1,5 +1,4 @@
-
-    function showLastModified()  {
+function showLastModified()  {
     const lastModified = new Date(document.lastModified);
     const options = { year: 'numeric', month: 'long', day: 'numeric', hour: '2-digit', minute: '2-digit', second: '2-digit' };
     const formattedDate = lastModified.toLocaleDateString('en-US', options);
