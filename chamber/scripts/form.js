e83fc561aaa7c637a2299d00bb5c3531501b08a0
Joseph Ochieng Odongo <josephodongo99@gmail.com>
2026-02-07 23:00:39 +0300

    effct

---
+++ chamber/scripts/form.js
@@ -0,0 +1,23 @@
+JSON
+
+
+    console.log(myInfo.get('first_name'));
+    console.log(myInfo.get('last_name '));
+    console.log(myInfo.get('phone'));
+    console.log(myInfo.get('email'));
+    console.log(myInfo.get('membership_level'));
+    console.log(myInfo.get('oranization'));
+    console.log(myInfo.get('thankyou.html'));
+    console.log(myInfo.get('selcter'));
+
+     document.querySelector('#result').innerHTML = 
+    <p>Membership level for ${myInfo.get('first')} ${myInfo.get('last')}</p>
+
+    
+    
+
+
+
+
+
+
