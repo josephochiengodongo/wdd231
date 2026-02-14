6d9ce476181006bbf8951c22026c9e5cc4c94124
Joseph Ochieng Odongo <josephodongo99@gmail.com>
2026-02-14 13:16:13 +0300

    effect

--- chamber/scripts/scripts.js
+++ chamber/scripts/scripts.js
@@ -1,2 +1,24 @@
 import { places } from "../Data/places.mjs";
-console.log()
+console.log(places)=[]
+import { places } from './data/places.mjs';
+
+const container = document.querySelector('.container');
+
+places.forEach((place, index) => {
+
+  const card = document.createElement('section');
+  card.classList.add('card');
+  card.style.gridArea = `card${index + 1}`;
+
+  card.innerHTML = `
+    <h2>${place.name}</h2>
+    <figure>
+      <img src="${place.image}" alt="${place.name}">
+    </figure>
+    <address>${place.address}</address>
+    <p>${place.description}</p>
+    <button>Learn More</button>
+  `;
+
+  container.appendChild(card);
+});
