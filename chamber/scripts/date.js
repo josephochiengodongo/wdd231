70369855e0e52a3860559c502536364d4073a0c2
Joseph Ochieng Odongo <josephodongo99@gmail.com>
2026-02-14 16:57:28 +0300

    effect

---
+++ chamber/scripts/date.js
@@ -0,0 +1,19 @@
+// milliseconds to days constant = 1000 ms/s * 60 s/m * 60 m/h * 24 h/day
+const msToDays = 86400000;
+// today's date
+const theDateToday = new Date();
+
+// initialize display elements
+const todayElement = document.querySelector("#today");                                                                   
+const daysElement = document.querySelector("#daysleft");
+
+// processing                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                
+const today = Date.now()
+if (theDateToday.getMonth() == 11 && theDateToday.getDate() > 25) {
+	
+}
+// find difference between epoch times in ms and convert to days
+let daysleft = (christmasDate.getTime() - Date.now()) / msToDays;
+
+todayElement.textContent = today;
+daysElement.textContent = `${daysleft.toFixed(0)} days`;
