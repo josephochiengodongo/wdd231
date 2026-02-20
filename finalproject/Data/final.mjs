34c0b771aafb65a7e223643794cf793062faa005
Joseph Ochieng Odongo <josephodongo99@gmail.com>
2026-02-20 18:22:03 +0300

    effect

--- finalproject/Data/final.mjs
+++ finalproject/Data/final.mjs
@@ -60,5 +60,99 @@ export const places=[
     "address": "Rift Valley Province, Kenya",
     "contact": "0756 780 987",
     "image_url": "https://upload.wikimedia.org/wikipedia/commons/2/25/Hells_Gate_National_Park_Kenya.jpg"
-  }
+  },
+  {
+    "name": "Amboseli National Park",
+    "county": "Kajiado",
+    "type": "National Park",
+    "url": "https://www.kws.go.ke/amboseli-national-park"
+  },
+  {
+    "name": "Tsavo East National Park",
+    "county": "Taita-Taveta",
+    "type": "National Park",
+    "url": "https://www.kws.go.ke/tsavo-east-national-park"
+  },
+  {
+    "name": "Tsavo West National Park",
+    "county": "Taita-Taveta",
+    "type": "National Park",
+    "url": "https://www.kws.go.ke/tsavo-west-national-park"
+  },
+  {
+    "name": "Lake Nakuru National Park",
+    "county": "Nakuru",
+    "type": "National Park",
+    "url": "https://www.kws.go.ke/lake-nakuru-national-park"
+  },
+  {
+    "name": "Mount Kenya National Park",
+    "county": "Kirinyaga",
+    "type": "National Park",
+    "url": "https://www.kws.go.ke/mount-kenya-national-park"
+  },
+  {
+    "name": "Diani Beach",
+    "county": "Kwale",
+    "type": "Beach",
+    "url": "https://www.magicalkenya.com/destinations/diani/"
+  },
+  {
+    "name": "Lamu Old Town",
+    "county": "Lamu",
+    "type": "Historic Town",
+    "url": "https://whc.unesco.org/en/list/1055/"
+  },
+  {
+    "name": "Samburu National Reserve",
+    "county": "Samburu",
+    "type": "National Reserve",
+    "url": "https://www.samburu.go.ke/tourism/"
+  },
+  {
+    "name": "Lake Naivasha",
+    "county": "Nakuru",
+    "type": "Freshwater Lake",
+    "url": "https://www.magicalkenya.com/destinations/lake-naivasha/"
+  },
+  {
+    "name": "Watamu Marine National Park",
+    "county": "Kilifi",
+    "type": "Marine Park",
+    "url": "https://www.kws.go.ke/watamu-marine-national-park"
+  },   
+  {
+    "name": "Aberdare National Park",
+    "county": "Nyeri",
+    "type": "National Park",
+    "url": "https://www.kws.go.ke/aberdare-national-park"
+  },
+  {
+    "name": "Lake Turkana",
+    "county": "Marsabit",
+    "type": "Desert Lake",
+    "url": "https://whc.unesco.org/en/list/801/"
+  }, 
+  {
+    "name": "Shimba Hills National Reserve",
+    "county": "Kwale",
+    "type": "National Reserve",
+    "url": "https://www.kws.go.ke/shimba-hills-national-reserve"
+  },
+  {
+    "name": "Ol Pejeta Conservancy",
+    "county": "Laikipia",
+    "type": "Wildlife Conservancy",
+    "url": "https://www.olpejetaconservancy.org/"
+  },
+  {
+    "name": "Karura Forest",
+    "county": "Nairobi",
+    "type": "Urban Forest",
+    "url": "https://www.friendsofkarura.org/"
+  },
+  
+   
 ]
+    
+  
