61f55596d88fe0224956e7d22ee83afd414c4f1c
Joseph Ochieng Odongo <josephodongo99@gmail.com>
2026-04-08 08:21:51 +0300

    effect

--- chamber/scripts/getdates.js
+++ chamber/scripts/getdates.js
@@ -1,7 +1,3 @@
 const currentYear = new Date().getFullYear();
-
-// Output current year in footer first paragraph
 document.getElementById("currentyear").innerHTML = currentYear;
-
-// Output last modified date in footer second paragraph
 document.getElementById("lastModified").innerHTML = document.lastModified;// Get current year
