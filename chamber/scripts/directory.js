d0b2d52a78c06b3496f75de5e2bb2496907aa0ef
Joseph Ochieng Odongo <josephodongo99@gmail.com>
2026-04-14 20:24:31 +0300

    effect

--- chamber/scripts/directory.js
+++ chamber/scripts/directory.js
@@ -39,3 +39,16 @@ listBtn.addEventListener("click", () => {
 });
 
 getMembers();
+// Timestamp
+document.getElementById("timestamp").value = new Date().toISOString();
+
+// Modal functions
+function openModal(id) {
+  document.getElementById(id).style.display = "block";
+}
+
+function closeModal(id) {
+  document.getElementById(id).style.display = "none";
+}
+
+
