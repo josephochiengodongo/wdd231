7e7ab038ad05c45f1d0623a4ded41ca3e6b1deb1
Joseph Ochieng Odongo <josephodongo99@gmail.com>
2026-05-27 17:48:19 +0300

    efffect

---
+++ finalproject/scripts/places.js
@@ -0,0 +1,22 @@
+const places = [
+  {
+    name: "Maasai Mara National Reserve",
+    type: "National Park",
+    address: "Maasai Mara, Narok County, Kenya",
+    url: "https://upload.wikimedia.org/wikipedia/commons/0/04/Maasai_Mara_Lion_Pride.jpg"
+  },
+  {
+    name: "Nairobi National Park",
+    type: "National Park",
+    address: "Along Lang'ata Road, Nairobi, Kenya",
+    url: "https://upload.wikimedia.org/wikipedia/commons/4/43/Nairobi_National_Park_lions.jpg"
+  },
+  {
+    name: "Diani Beach",
+    type: "Beach",
+    address: "Diani, Kenya",
+    url: "https://upload.wikimedia.org/wikipedia/commons/5/5e/Diani_Beach_Kenya.jpg"
+  }
+];
+
+export default places;
