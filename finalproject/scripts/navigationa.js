aaab862dca652be48e6b9de336a5f7fb899c7028
Joseph Ochieng Odongo <josephodongo99@gmail.com>
2026-06-17 23:33:12 +0300

    effect

--- finalproject/scripts/navigationa.js
+++
@@ -1,6 +0,0 @@
-const menuButton = document.querySelector(".toggle-menu");
-const nav = document.querySelector("nav");
-
-menuButton.addEventListener("click", () => {
-    nav.classList.toggle("open");
-});
