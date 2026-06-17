d6c038820bd3cc1f395083e0f1fa550e5eefc955
Joseph Ochieng Odongo <josephodongo99@gmail.com>
2026-06-17 23:08:45 +0300

    effect

--- finalproject/scripts/now.js
+++
@@ -1,6 +0,0 @@
-const menuButton = document.querySelector(".toggle-menu");
-const navigation = document.querySelector("header nav");
-
-menuButton.addEventListener("click", () => {
-    navigation.classList.toggle("open");
-});
